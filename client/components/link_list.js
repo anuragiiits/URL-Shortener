9faf926124b89ce7fd611b6ceab4f980aced2ca1
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-22 01:25:11 +0530

    Published and Subscribe

---
+++ client/components/link_list.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Links } from '../../imports/collections/links';
+
+class LinkList extends Component{
+    render(){
+        return(
+            <div>List of links to click</div>
+        );
+    };
+};
+
+export default createContainer(()=>{
+    Meteor.subscribe('links');
+    return {links: Links.find({}).fetch()};
+}, LinkList);
