4638e22b878e86a0a771a77d8f08f534aceb501c
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-22 02:01:08 +0530

    Data added to database. Testable

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import LinkCreate from './components/link_create';
+import LinkList from './components/link_list';
 import { Link } from '../imports/collections/links';
 
 const App = ()=>{
@@ -9,6 +10,7 @@ const App = ()=>{
     <div>
       <Header />
       <LinkCreate />
+      <LinkList />
     </div>
   );
 };
